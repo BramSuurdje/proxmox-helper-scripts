9a0ad40e1f498404fa622ef3dda46b291af04d6a
Bram Suurd <78373894+BramSuurdje@users.noreply.github.com>
2024-05-02 14:15:46 +0200

    Refactor layout.tsx to import React and remove unused imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
@@ -7,6 +6,7 @@ import Navbar from "@/components/Navbar";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import Footer from "@/components/Footer";
+import React from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
