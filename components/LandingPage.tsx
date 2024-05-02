124620a5109e1fe31effda6b643097265ab744d9
Bram Suurd <78373894+BramSuurdje@users.noreply.github.com>
2024-05-02 14:43:09 +0200

    Update landing page layout and content to fix background and mask gradient

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -7,8 +7,8 @@ import Link from "next/link";
 export default function LandingPage() {
   return (
     <div className="flex justify-center">
-      <div className="relative flex h-screen w-5/6 flex-col items-center justify-center bg-grid-black/[0.2] dark:bg-grid-white/[0.2]">
-        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
+      <div className="relative flex h-screen w-full flex-col items-center justify-center bg-grid-black/[0.1] dark:bg-grid-white/[0.1]">
+        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_0%,black)] dark:bg-black"></div>
         <div className="flex animate-fade-up flex-col items-center justify-center">
           <Image src="/logo.png" alt="proxmox" width={150} height={150} />
           <h1 className="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-4 text-4xl font-bold text-transparent sm:text-5xl">
