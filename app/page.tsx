4f9ecac165ceef51ca67ad74749f68808d562b6f
Bram Suurd <78373894+BramSuurdje@users.noreply.github.com>
2024-05-02 14:14:33 +0200

    Remove unused imports and components in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,4 @@
-'use client'
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import React from "react";
-import Image from "next/image";
-import { Typewriter } from "react-simple-typewriter";
 import LandingPage from "@/components/LandingPage";
-import Footer from "@/components/Footer";
 
 function page() {
   return (
