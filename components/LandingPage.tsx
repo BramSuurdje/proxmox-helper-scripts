7b1e5f003c9b56cae5669649dccc79c1d068bc4f
Bram Suurd <78373894+BramSuurdje@users.noreply.github.com>
2024-05-02 14:15:36 +0200

    Update landing page layout and content to use client-side rendering

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -1,5 +1,5 @@
+'use client'
 import Image from "next/image";
-import React from "react";
 import { Typewriter } from "react-simple-typewriter";
 import { Button } from "./ui/button";
 import Link from "next/link";
