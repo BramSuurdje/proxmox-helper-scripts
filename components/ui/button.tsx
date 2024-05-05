5a3f7cce284681a02908d175d0a9e889b7ad21cf
Bram Suurd <78373894+BramSuurdje@users.noreply.github.com>
2024-05-05 19:53:01 +0200

    Add NEXT_PUBLIC_POCKETBASE_URL environment variable

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -13,9 +13,9 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:border-[#0080C4] hover:text-accent-foreground",
+          "border border-input bg-background hover:bg-accent hover:border-primary hover:text-accent-foreground",
         secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+          "bg-secondary border-secondary text-secondary-foreground hover:bg-secondary/80 hover:border-primary",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
