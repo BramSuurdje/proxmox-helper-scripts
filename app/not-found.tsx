244bf34e9e6098057631c997f4d0ab834eb7e3b8
Bram Suurd <78373894+BramSuurdje@users.noreply.github.com>
2024-05-16 19:43:57 +0200

    Fix typo in NotFoundPage component

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -7,7 +7,7 @@ function NotFoundPage() {
       <div className="text-center">
         <h1 className="text-9xl font-black">404</h1>
         <p className="text-2xl font-bold tracking-tight sm:text-4xl">Uh-oh!</p>
-        <p className="mt-4 text-gray-500">We can't find that page.</p>
+        <p className="mt-4 text-gray-500">We can&apos;t find that page.</p>
 
         <Button asChild className="mt-6 text-foreground">
           <Link href="/">Go Back Home</Link>
