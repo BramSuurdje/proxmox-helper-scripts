a9af4709858c8503189dfb46ace962019cf31f2a
Bram Suurd <78373894+BramSuurdje@users.noreply.github.com>
2024-05-17 22:16:50 +0200

    Revert "chore: added some margin to the bottom of the /scripts page to improve navigation on mobile and desktop"
    This reverts commit a66fca5a59118dd1b923ff92cb2dd571e0ddc4a8.

--- app/scripts/page.tsx
+++ app/scripts/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
         className="animate-fade-in absolute  inset-0 -z-10"
         quantity={100}
       />
-      <div className="mt-20 flex sm:px-7 xl:px-0 sm:mb-16 mb-10">
+      <div className="mt-20 flex sm:px-7 xl:px-0">
         <div className="hidden sm:flex">
           <ScriptBrowser />
         </div>
