6ec04b9f0ade989b1238f4ac1c0c5bc4972b89c6
Bram Suurd <78373894+BramSuurdje@users.noreply.github.com>
2024-05-21 16:41:19 +0200

    Update Footer component to display current year and use "Copyright" instead of "tteck. All rights reserved."

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,15 @@
 import Link from "next/link";
 import React from "react";
 
+const year = new Date().getFullYear();
+
 export default function Footer() {
   return (
-    <div className="glass w-full border-b bg-background/50 border-t py-6 text-center">
-      tteck. All rights reserved. |{" "}
-      <Link className="hover:underline" href="/terms-of-use">Terms of Use</Link>
+    <div className="glass w-full border-b border-t bg-background/50 py-6 text-center">
+      Copyright {year} tteck. All rights reserved. |{" "}
+      <Link className="hover:underline" href="/terms-of-use">
+        Terms of Use
+      </Link>
     </div>
   );
 }
