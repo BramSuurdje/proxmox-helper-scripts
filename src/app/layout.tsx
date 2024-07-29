a537a4f150cebc1a603a54bfe14bb0bed487e5bb
Bram Suurd <contact@bramsuurd.nl>
2024-07-30 00:01:44 +0200

    new next project + dependencies

---
+++ src/app/layout.tsx
@@ -0,0 +1,56 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Proxmox VE Helper-Scripts",
+  generator: "Next.js",
+  applicationName: "Proxmox VE Helper-Scripts",
+  referrer: "origin-when-cross-origin",
+  keywords: [
+    "Proxmox VE",
+    "Helper-Scripts",
+    "tteck",
+    "helper",
+    "scripts",
+    "proxmox",
+    "VE",
+  ],
+  authors: { name: "Bram Suurd" },
+  creator: "Bram Suurd",
+  publisher: "Bram Suurd",
+  description:
+    "A Re-designed Front-end for the Proxmox VE Helper-Scripts Repository. Featuring all the scripts you need to streamline your homelab. With a modern and clean design.",
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  metadataBase: new URL("https://proxmox-helper-scripts.vercel.app/"),
+  openGraph: {
+    title: "Proxmox VE Helper-Scripts",
+    description:
+      "A Re-designed Front-end for the Proxmox VE Helper-Scripts Repository. Featuring all the scripts you need to streamline your homelab. With a modern and clean design.",
+    url: "/defaultimg.png",
+    images: [
+      {
+        url: "https://proxmox-helper-scripts.vercel.app/defaultimg.png",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+};
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
