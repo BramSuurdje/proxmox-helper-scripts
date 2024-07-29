5c59814c088e6b75fd58e14f71e89175ea41dabe
Bram Suurd <contact@bramsuurd.nl>
2024-07-29 23:55:08 +0200

    remove all

--- lib/time.ts
+++
@@ -1,7 +0,0 @@
-export function extractDate(dateString: string): string {
-  const date = new Date(dateString);
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
-  return `${year}-${month}-${day}`;
-}
