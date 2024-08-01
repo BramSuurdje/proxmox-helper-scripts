2418c1ba7fc4bbd8427dc2405755ced915143f7d
Bram Suurd <contact@bramsuurd.nl>
2024-08-01 02:12:29 +0200

    refactor: Add lazy loading for images in DashboardLayout component

--- src/app/catalogue/layout.tsx
+++ src/app/catalogue/layout.tsx
@@ -1,4 +1,5 @@
 import Sidebar from "@/components/Sidebar";
+import { Suspense } from "react";
 
 export default function DashboardLayout({
   children, // will be a page or nested layout
@@ -7,8 +8,10 @@ export default function DashboardLayout({
 }) {
   return (
     <section className="flex justify-center">
-      <div className="max-w-7xl w-full flex">
-        <Sidebar />
+      <div className="max-w-8xl mx-6 w-full flex">
+        <Suspense fallback={<div>Loading...</div>}>
+          <Sidebar />
+        </Suspense>
         {children}
       </div>
     </section>
--- src/app/catalogue/page.tsx
+++ src/app/catalogue/page.tsx
@@ -26,6 +26,7 @@ export default async function page() {
         ))}
       </ul>
 
+
     </div>
   );
 }
