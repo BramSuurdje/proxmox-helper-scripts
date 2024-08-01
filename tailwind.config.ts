eba86ea5ab06330531a3e6ded7e0cb94cc02dfce
Bram Suurd <contact@bramsuurd.nl>
2024-08-01 02:11:37 +0200

    chore: Update tailwind.config.ts to extend maxWidth with "8xl" size

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ const config = {
       },
     },
     extend: {
+      maxWidth: {
+        "8xl": "90rem",
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
