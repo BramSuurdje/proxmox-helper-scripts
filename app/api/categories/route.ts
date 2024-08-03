fcfe4f8f693e0350391b0a0f47b78601a9d5d0ed
Bram Suurd <contact@bramsuurd.nl>
2024-08-03 14:46:51 +0200

    feat: Add API route for fetching categories

---
+++ app/api/categories/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+import { pb } from "@/lib/pocketbase";
+import { Category } from "@/lib/types";
+
+export async function GET(req: NextRequest) {
+  try {
+    const response = await pb.collection("categories").getFullList({
+      expand: "items",
+      sort: "order",
+    });
+
+    const res = NextResponse.json(response as unknown as Category[]);
+    return res;
+  } catch (error) {
+    console.error("Error fetching categories:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch categories" },
+      { status: 500 },
+    );
+  }
+}
