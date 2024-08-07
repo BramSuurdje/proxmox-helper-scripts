23098003aeb72d444b4db5791107986eeb9c386e
Bram Suurd <78373894+BramSuurdje@users.noreply.github.com>
2024-08-07 22:35:29 +0200

    refactor: Remove unused code in utils.tsx

--- src/lib/utils.tsx
+++ src/lib/utils.tsx
@@ -6,25 +6,3 @@ import { ClipboardCheck } from "lucide-react";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export const handleCopy = (type: string, value: any) => {
-    navigator.clipboard.writeText(value);
-
-    let warning = localStorage.getItem("warning");
-
-    if (warning === null) {
-      localStorage.setItem("warning", "1");
-      setTimeout(() => {
-        toast.error(
-          "be careful when copying scripts from the internet. Always remember check the source!",
-          { duration: 8000 },
-        );
-      }, 500);
-    }
-
-  toast.success(
-    `copied ${type} to clipboard`, {
-        icon: <ClipboardCheck className="h-4 w-4" />,
-      }
-    );
-  };
