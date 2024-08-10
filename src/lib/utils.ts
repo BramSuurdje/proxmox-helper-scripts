416df4895ed97ac322cfa738ef699e91e3e8a3f3
Bram Suurd <contact@bramsuurd.nl>
2024-08-10 16:15:20 +0200

    refactor: Remove unused code in utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,5 @@
 import { type ClassValue, clsx } from "clsx";
-import { toast } from "sonner";
 import { twMerge } from "tailwind-merge";
-import { ClipboardCheck } from "lucide-react";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
