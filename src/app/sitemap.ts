465c13575c1247998f6e665461bab94cf0e9cdc2
Bram Suurd <contact@bramsuurd.nl>
2024-09-10 09:58:45 +0200

    changed urls from old to new across repo

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,13 +3,13 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://proxmox-helper-scripts.vercel.app",
+      url: "https://proxmoxve-scripts.com",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.8,
     },
     {
-      url: "https://proxmox-helper-scripts.vercel.app/scripts",
+      url: "https://proxmoxve-scripts.com/scripts",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
