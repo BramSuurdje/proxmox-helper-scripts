a6f3333fcb95111b4b693518e804e17e84e3e3dc
Bram Suurd <78373894+BramSuurdje@users.noreply.github.com>
2024-09-14 19:46:13 +0200

    feat: add manifest file for Proxmox VE Helper-Scripts with app metadata and icons configuration

---
+++ src/app/manifest.ts
@@ -0,0 +1,23 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Proxmox VE Helper-Scripts",
+    short_name: "Proxmox VE Helper-Scripts",
+    description:
+      "A Re-designed Front-end for the Proxmox VE Helper-Scripts Repository. Featuring over 150+ scripts to help you manage your Proxmox VE environment.",
+    theme_color: "#030712",
+    background_color: "#030712",
+    display: "standalone",
+    orientation: "portrait",
+    scope: "/",
+    start_url: "/",
+    icons: [
+      {
+        src: "/logo.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
